76db3414e419f55e7af1ac195f3de9698f0ca965
Ravinder Jangra <ravinderjangra@live.com>
2019-01-22 18:47:05 +0530

    feat/SafeTodoExample: Refactor code and simplify

---
+++ SafeTodoExample/SafeTodoExample/SafeTodoExample/Helpers/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace SafeTodoExample.Helpers
+{
+    public class Constants
+    {
+        public const string AppId = "net.maidsafe.examples.todo";
+        public const string AppName = "Safe Todo";
+        public const string Vendor = "MaidSafe.net Ltd";
+    }
+}
